ac35e72aba2ad2febe35e4f936fed1aa18cf8b21
Patrick Walujan <patrickwalujan.dev@gmail.com>
2023-04-30 11:22:56 +0800

    add login page

--- script.js
+++ script.js
@@ -25,3 +25,16 @@ const displayCardData = (data) => {
     cardContainer.appendChild(cardElement);
   });
 };
+
+// Login form
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
